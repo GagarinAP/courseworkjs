4b502a4473cd2efc1bcec412d8aa264dae3052bd
GagarinAP <gagarin.web@gmail.com>
2016-07-06 12:43:14 +0300

    add file and folder OSBB application

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
 var express = require('express');
 var bodyParser = require("body-parser");
-var app = express();
+var path = require('path');
+//var indexPage = require('./views/indexPage.js');
+/*var keysModule = require('./models/keysModule.js');
+var appView = require('./views/appView.js');*/
 
+var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
-app.set('port',3000);
+
+
+
+
+var myLogger = function (req, res, next) {
+  console.log('LOGGED');
+  next();
+};
+
+app.use(myLogger);
+
+app.get('/', function (req, res) {
+  res.send('fdgsdgsd!');
+});
+/*app.get('/workers', function (req, res) {
+    res.send(keysModule.getAllWorkers());
+});
+
+app.get('/search', function (req, res) {
+  res.send(appView.searchRecord(req.query));
+});
+
+app.get('/chart', function (req, res) {
+  res.send(appView.getChartData());
+});
+
+app.post('/record', function (req, res) {
+    res.send(appView.addRecord(req.body)) ;
+});*/
 
 
 app.listen(3000, function () {
-  console.log('Application is run localhost:' + app.get('port'));
+  console.log('OSBB Application listening on port 3000!');
 });
